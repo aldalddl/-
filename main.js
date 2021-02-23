814d953b78f5158fb4de2bc2c9ae526de148d4c9
Minji Kang <aldalddl2007@gmail.com>
2021-02-23 11:31:37 +0900

    2

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 var comment=false;
 var folder_list=true;
 
-var folder =[]; //firebase에서 annotation의 값을 받아와서 저장할 array
 var count = 0; //annotation의 개수를 카운트
-var user_name = "user"; // firebase에서 값 받아와서 저장
 var folder_name;
 var comment_list_div;
 
@@ -14,40 +12,91 @@ var add_folder_isClicked = false;
 
 window.onload = function () {
 
-	
+	// 팝업 창 열기
 	function onClick() {
+		console.log("clicked");
         document.querySelector('.modal_wrap').style.display ='block';
         document.querySelector('.black_bg').style.display ='block';
+
     }   
+	// 팝업 창 닫기
     function offClick() {
         document.querySelector('.modal_wrap').style.display ='none';
         document.querySelector('.black_bg').style.display ='none';
+
     }
  
-    document.getElementById('new_file_btn').addEventListener('click', onClick);
-    document.querySelector('.modal_close').addEventListener('click', offClick);
+//    document.getElementById('new_file_btn').addEventListener('click', onClick);
+//	document.getElementById('new_file_btn').onclick = function() { 
+//		onClick();
+//
+//	}
+	
+	 document.getElementById("new_file_btn").addEventListener("click", function(){   
+		 console.log("1");
+		 onClick();
+//		 document.getElementById("new_file_btn").disabled = false; 
+//	document.getElementById("new_file_btn").removeAttribute("disabled");
+		 console.log("2");
+	 });
 	
 
-    document.getElementById("OK_btn").onclick = function() {   
-		offClick(); //팝업 창 닫는 함수
-		folder_name = document.getElementById('folder_name').value;
-//		console.log("folder_name is " + folder_name);
+    document.querySelector('.modal_close').addEventListener('click', offClick);
 
-		add_folder(count); //새 폴더생성하는 함수
-		count++; //생성되는 새폴더 갯수 새는 변수
-		
-		document.getElementById("OK_btn").disabled = true; 
-//		add_folder(folder_name);
-//		console.log("OK_btn is finised");
-    }
+
+
+    document.getElementById("OK_btn").addEventListener("mousedown", function(){   
+		count++; // 새 폴더 갯수 카운트
+
+		add_folder(count); 
+
+		offClick(); 
+		document.getElementById("OK_btn")
+    });
+	
+
 }
 
+//새 폴더 생성
 function add_folder(count) {
-  	console.log("count is " + count);
-		folder_name = document.getElementById('folder_name').value;
-		console.log("folder_name is " + folder_name);
-		var target = document.querySelector(".newfolder_list").cloneNode(true); //all.mytable.cloneNode(true)
-      	document.querySelector(".file_list").innerHTML += target.innerHTML;
+	console.log("count is " + count);
+	folder_name = document.getElementById('folder_name').value;
+	console.log("folder_name is " + folder_name);
+	
+	var file = document.getElementById('new_project_filechoose_btn');
+	//파일 경로.
+	var filePath = file.value;
+	//전체경로를 \ 나눔.
+	var filePathSplit = filePath.split('\\'); 
+	//전체경로를 \로 나눈 길이.
+	var filePathLength = filePathSplit.length;
+	//마지막 경로를 .으로 나눔.
+	var fileNameSplit = filePathSplit[filePathLength-1].split('.');
+	//파일명 : .으로 나눈 앞부분
+	var fileName = fileNameSplit[0];
+	//파일 확장자 : .으로 나눈 뒷부분
+	var fileExt = fileNameSplit[1];
+	//파일 크기
+	var fileSize = file.files[0].size;
+
+	console.log('파일 경로 : ' + filePath);
+	console.log('파일명 : ' + fileName);
+	console.log('파일 확장자 : ' + fileExt);
+	console.log('파일 크기 : ' + fileSize);
+	
+//	document.getElementById('folder').innerHTML=folder_name;
+//	
+//	var target = document.querySelector(".newfolder_list").cloneNode(true); 
+//	document.querySelector(".file_list").innerHTML += target.innerHTML;	
+	
+	document.getElementById("folder_column").innerHTML +=
+		'<td><div> <table class="newfolder_list" id="newfolder_list" style="float:left;"> <tr><td class="newproject_text" id="folder">' + folder_name + '</td></tr><tr><td class="newproject_text" id="folder_maker">만든 이 </td></tr><tr><td class="newproject_text" id="folder_imagename">' + fileName+'.'+fileExt+ '</td></tr></table></div></td>';
+		console.log("new_file_list");
+	
+//		 document.getElementById("new_file_btn").disabled = false; 
+//	document.getElementById("new_file_btn").removeAttribute("disabled");
+
+
 }
 
 
